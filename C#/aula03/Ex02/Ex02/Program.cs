6c8dbae8d86646579fc788c383aa39a4dbf0e39f
PEDRO LUIZ HOLUBOSKI JUNIOR <pedro.lhjunior@senacsp.edu.br>
2024-08-08 20:20:34 -0300

    .

---
+++ C#/aula03/Ex02/Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿string usuario; string senha;
+
+Console.WriteLine("Informe o usuário");
+usuario = Console.ReadLine();
+Console.WriteLine("Informe a senha");
+senha = Console.ReadLine();
+
+if (usuario == "matuzalem" && senha == "123a")
+{
+    Console.WriteLine("Seja bem vindo usuário Matuzalem");
+}
+else if (usuario == "benegundes" && senha == "vaifilao")
+{
+    Console.WriteLine("Seja bem vindo usuário Benegunde");
+}
+else if (usuario == "judith" && senha == "genoveva")
+{
+    Console.WriteLine("Seja bem vindo usuário Judith");
+}
+else
+{
+    Console.WriteLine("Usuário ou senha inválido");
+}
---
+++ C#/aula03/ex01/ex01/Program.cs
@@ -0,0 +1,46 @@
+﻿string produto; float qtde; float valor; int tipo; float total; float total2;
+
+Console.Write("Informe Produto: ");
+produto = Console.ReadLine();
+
+Console.Write("Informe Qtde: ");
+qtde = float.Parse(Console.ReadLine());
+
+Console.Write("Informe Valor Unitário: ");
+valor = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe tipo de pagamento: ");
+Console.WriteLine("1 - PIX");
+Console.WriteLine("2 - Crédito");
+Console.WriteLine("3 - Boleto");
+Console.WriteLine("4 - Dinheiro");
+tipo = int.Parse(Console.ReadLine());
+
+total = qtde * valor;
+
+if (tipo == 1)
+{
+    total2 = total * 0.95F;
+}
+else if (tipo == 2)
+{
+    total2 = total * 1.1F;
+}
+else if (tipo == 3)
+{
+    total2 = total * 1.15F;
+}
+else if (tipo == 4)
+{
+    total2 = total;
+}
+else
+{
+    Console.WriteLine("Opção inválida");
+    return;
+}
+
+Console.WriteLine("");
+Console.WriteLine("Total Geral: " + total.ToString("C"));
+Console.WriteLine("Total a Pagar: " + total2.ToString("C"));
+
